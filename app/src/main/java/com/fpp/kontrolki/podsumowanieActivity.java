211252a696c1dee677e223ed9151a34176400e98
Filippo0420 <fpawelec0@gmail.com>
2021-12-07 16:07:47 +0100

    komit

--- app/src/main/java/com/fpp/kontrolki/podsumowanieActivity.java
+++ app/src/main/java/com/fpp/kontrolki/podsumowanieActivity.java
@@ -8,16 +8,38 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.CompoundButton;
 import android.widget.Switch;
+import android.widget.TextView;
 
 public class podsumowanieActivity extends AppCompatActivity {
 
     private Button dalej;
     private Switch aSwitch;
 
+    private TextView jkawa, jwielkosc, jdodatek, jadres, jtelefon;
+
+    String kawa, wielkosc, dodatek, adres, telefon;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_podsumowanie);
+
+        Intent iin= getIntent();
+        Bundle b = iin.getExtras();
+
+        if(b!=null)
+        {
+            adres =(String) b.get("Adres");
+            telefon =(String) b.get("Telefon");
+            dodatek =(String) b.get("Dodatek");
+            wielkosc =(String) b.get("Wielkosc");
+            kawa =(String) b.get("Kawa");
+        }
+
+        jkawa = findViewById(R.id.tekst2);
+
+        jkawa.setText("Kawa "+ kawa + " " + wielkosc + " z dodatkiem: ");
+
 
         dalej = findViewById(R.id.dalej);
         aSwitch = findViewById(R.id.regulamin);
