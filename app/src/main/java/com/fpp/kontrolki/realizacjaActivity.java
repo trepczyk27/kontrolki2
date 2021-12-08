20625f46becb1a06d97b44542a389a7da858dff4
Filippo0420 <fpawelec0@gmail.com>
2021-12-08 21:02:44 +0100

    gotowe chyba tak

--- app/src/main/java/com/fpp/kontrolki/realizacjaActivity.java
+++ app/src/main/java/com/fpp/kontrolki/realizacjaActivity.java
@@ -3,12 +3,28 @@ package com.fpp.kontrolki;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.RatingBar;
+import android.widget.Toast;
 
 public class realizacjaActivity extends AppCompatActivity {
+    private RatingBar ocena;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_realizacja);
+
+        ocena = findViewById(R.id.ocena);
+
+        ocena.setOnRatingBarChangeListener(new RatingBar.OnRatingBarChangeListener() {
+            @Override
+            public void onRatingChanged(RatingBar ratingBar, float v, boolean b) {
+                if(v < 4){
+                    Toast.makeText(realizacjaActivity.this, "Dziękujemy za opinię", Toast.LENGTH_SHORT).show();
+                }else{
+                    Toast.makeText(realizacjaActivity.this, "Dziękujemy za dobrą opinię", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 }
